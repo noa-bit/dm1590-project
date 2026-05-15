371e29f03281c58ebf7d2013d36532aef7039b3b
erikesshagen <erik.esshagen@gmail.com>
2026-05-15 10:02:23 +0200

    Further work on svm

--- SVM.py
+++ SVM.py
@@ -2,26 +2,40 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.metrics import classification_report, accuracy_score
+
+class SVM:
+    def __init__(self, csv_name):
+        # Load CSV
+        self.df = pd.read_csv(csv_name)
+
+        # Features and target
+        self.X = self.df.drop('Label', axis=1).values 
+        self.y = self.df["Label"].values
+
+        # Placeholders
+        self.scaler = None
+        self.model = None
+
+    def svm_training(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42, stratify=self.y)
+
+        self.scaler = StandardScaler()
+
+        X_train_scaled = self.scaler.fit_transform(X_train)
+        X_test_scaled = self.scaler.transform(X_test)
+
+        self.model = SVC(kernel='rbf')
+
+        self.model.fit(X_train_scaled, y_train)
+
+        y_pred = self.model.predict(X_test_scaled)
+
+        print("Accuracy:", accuracy_score(y_test, y_pred))
+        print(classification_report(y_test, y_pred))
 
 
-def SVM(df):
+svm = SVM("features.csv")
+svm.svm_training()
 
-    X = df.drop(columns=["label"])   # features
-    y = df["label"]                  # target
-    
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
-
-    scaler = StandardScaler()
-
-    X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
-    svm_model = SVC(kernel="rbf", C=1.0, gamma="scale")
-    svm_model.fit(X_train_scaled, y_train)
-    y_pred = svm_model.predict(X_test_scaled)
-    
-    print("Accuracy:", accuracy_score(y_test, y_pred))
-    print("\nConfusion Matrix:\n", confusion_matrix(y_test, y_pred))
-    print("\nReport:\n", classification_report(y_test, y_pred))
-    return svm_model, scaler, y_pred
+svm2 = PCA("features.csv")
